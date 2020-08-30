6c9baa54dfac980872bb70298fb4f244ff7e01db
ivansilvestre <ivanbsilvestre@gmail.com>
2020-08-30 12:13:13 +0100

    add api url and logic

---
+++ script.js
@@ -0,0 +1,17 @@
+async function getQuote() {
+  //proxyUrl to avoid CORS
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    getQuote();
+    console.log(error);
+  }
+}
+
+getQuote();
