6b4887e3b39b9a74f650514a0473a9cd8377c841
ivansilvestre <ivanbsilvestre@gmail.com>
2020-09-05 23:12:40 +0100

    add logic and loading

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-async function getQuote() {
-  //proxyUrl to avoid CORS
-  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-  const apiUrl =
-    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+const quoteContainer = document.getElementById("quote-generator");
+const quoteText = document.getElementById("quote-text");
+const author = document.getElementById("author");
+const btnNewQuote = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+let data = [];
+
+function addLoading() {
+  loader.style.display = "initial";
+  quoteContainer.style.display = "none";
+}
+
+function removeLoading() {
+  loader.style.display = "none";
+  quoteContainer.style.display = "initial";
+}
+
+function getNewQuote() {
+  const quote = data[Math.floor(Math.random() * data.length)];
+
+  quote.author === ""
+    ? (author.textContent = "Unknow")
+    : (author.textContent = quote.author);
+
+  quote.text.length > 100
+    ? quoteText.classList.add("long-quote")
+    : quoteText.classList.remove("long-quote");
+
+  quoteText.textContent = quote.text;
+}
+
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
 
   try {
-    const response = await fetch(proxyUrl + apiUrl);
-    const data = await response.json();
-    console.log(data);
+    addLoading();
+    const response = await fetch(apiUrl);
+    data = await response.json();
+
+    setTimeout(function () {
+      getNewQuote();
+      removeLoading();
+    }, 500);
   } catch (error) {
-    getQuote();
+    alert("oops... something went wrong");
     console.log(error);
   }
 }
 
-getQuote();
+btnNewQuote.addEventListener("click", getQuotes);
+
+getQuotes();
